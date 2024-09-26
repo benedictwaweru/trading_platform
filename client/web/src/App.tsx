28b6cc0b9fcb0a71e46e3e012f75eae4e18995a3
benedictwaweru <benedictwawerum@gmail.com>
2024-09-26 17:50:43 +0300

    Set up the auth route

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,10 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/user/HomePage";
+import Login from "./pages/user/auth/Login";
+import Signup from "./pages/user/auth/Signup";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route></Route>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/auth">
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/signup" element={<Signup />} />
+        </Route>
       </Routes>
     </Router>
   );
