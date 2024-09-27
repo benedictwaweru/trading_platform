3db849c1dddfcf4f388fcf8c68a7530d78120b95
benedictwaweru <benedictwawerum@gmail.com>
2024-09-27 23:29:03 +0300

    Use tailwind for styles

---
+++ client/web/src/pages/user/Dashboard.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="bg-gray-300 text-center font-bold italic underline text-3xl">
+      <h1>Tailwind CSS is awesome</h1>
+    </div>
+  );
+}
